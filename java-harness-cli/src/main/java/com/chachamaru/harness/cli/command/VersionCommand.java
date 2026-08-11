f64d85b6684b43f1de54dcec2bce013f7a03eda6
duxvfeng <395781596@qq.com>
2026-08-11 10:45:31 +0800

    提交

--- java-harness-cli/src/main/java/com/chachamaru/harness/cli/command/VersionCommand.java
+++ java-harness-cli/src/main/java/com/chachamaru/harness/cli/command/VersionCommand.java
@@ -1,16 +1,28 @@
 package com.chachamaru.harness.cli.command;
 
+import com.chachamaru.harness.cli.VersionInfo;
 import picocli.CommandLine.Command;
 
 /**
- * Print version
+ * Print version information for the Harness CLI.
+ *
+ * <p>This command outputs the current version of Harness, read from the
+ * bundled VERSION resource file. It provides a simple way to check the
+ * installed version without running other commands.</p>
  */
 @Command(name = "version",
-         description = "Print version")
+         mixinStandardHelpOptions = true,
+         description = "Print version information")
 public class VersionCommand implements Runnable {
+
+    /** Default constructor required by Picocli and GraalVM Native Image reflection. */
+    public VersionCommand() {
+        // Default constructor - required for reflection instantiation
+    }
 
     @Override
     public void run() {
-        System.out.println("VersionCommand executed");
+        String version = VersionInfo.getVersion();
+        System.out.println("harness " + version);
     }
 }
